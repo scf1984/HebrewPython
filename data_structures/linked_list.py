90d41a483e3cd1f65d06e7f5a25d565e8252a089
scf1984 <scf1984@gmail.com>
2020-06-22 09:26:51 +0300

    Finished linked list example.

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -10,6 +10,29 @@ class LinkedList:
         node.next = self.head
         self.head = node
 
+    def delete(self, value):
+        if self.head is None:
+            raise ValueError(f'value {value} is not in the list')
+        if self.head.value == value:
+            self.head = self.head.next
+        else:
+            prev = self.head
+            current = self.head.next
+            while current.value != value:
+                if current.next is None:
+                    raise ValueError(f'value {value} is not in the list')
+                current = current.next
+                prev = prev.next
+            prev.next = current.next
+
+    def __contains__(self, value):
+        current = self.head
+        while current is not None:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+
     def __str__(self):
         current = self.head
         strings = []
@@ -17,6 +40,13 @@ class LinkedList:
             strings.append(str(current.value))
             current = current.next
         return ' -> '.join(strings)
+
+    def __iter__(self):
+        # 6 -> 1 -> 12
+        current = self.head
+        while current is not None:
+            yield current.value
+            current = current.next
 
 
 class Node:
@@ -28,7 +58,11 @@ class Node:
 if __name__ == '__main__':
     ll = LinkedList()
     ll.add(6)
-    ll.add(10)
     ll.add(1)
-    print(ll)  # 1 -> 10 -> 6
+    ll.add(3)
+    ll.add(8)
+    for item in ll:
+        print(item)
+
+    # print(ll)  # 1 -> 10 -> 6
 
