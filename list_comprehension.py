fbf7b4d425ea69dddafa4e01d05fa3cf022e52c7
scf1984 <scf1984@gmail.com>
2020-10-08 12:06:00 +0300

    Adding list comprehension lesson.

---
+++ list_comprehension.py
@@ -0,0 +1,64 @@
+a = [1, 2, 3, 4, 5]
+
+
+##b = []
+##for i in a:
+##    b.append(i)
+##
+##b = [i for i in a]
+
+
+##b = []
+##for i in a:
+##    if i > 2:
+##        b.append(i)
+##
+##b = [i for i in a if i > 2]
+
+
+##b = []
+##for i in a:
+####    if i % 2 == 0:
+####        b.append(i)
+####    else:
+####        b.append(-i)
+##    b.append(i if i % 2 == 0 else -i)
+##    
+##
+##b = [i if i % 2 == 0 else -i for i in a]
+
+##b = []
+##for i in a:
+##    c = []
+##    for j in a:
+##        c.append(i * j)
+##    b.append(c)    
+##
+##b = [[i * j for j in a] for i in a]
+
+
+##b = []
+##for i in a:
+##    for j in a:
+##        b.append((i, j))
+##
+##b = [i * j for i in a for j in a]
+
+
+print(a)
+print()
+for row in b:
+    print(row)
+
+
+
+
+
+
+
+
+
+
+
+
+
